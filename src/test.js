988cac6dda5608d15ea402190cce36be590908f8
Doug <124453486+takilesi@users.noreply.github.com>
2023-07-25 17:40:45 -0400

    api call to get word info

--- src/test.js
+++ src/test.js
@@ -3,10 +3,9 @@ import './App.css';
 import axios from 'axios'; 
 
 function App() {
-  let wordTyped="cat"; 
+  let wordTyped=""; 
   const [word, setWord] = useState("")
   const [freq, setFreq] = useState(0)
-  const [def, setDef] = useState([])
   // const [country, setCountry] = useState([])
 
   const getData = async (word) => {
@@ -20,24 +19,11 @@ function App() {
       }
     };
     
-    const options2 = {
-      method: 'GET',
-      url: `https://wordsapiv1.p.rapidapi.com/words/${wordTyped}/definitions`,
-      headers: {
-        'X-RapidAPI-Key': '143280bb31msh90cc0477660eae7p193346jsnfc20028932fc',
-        'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-      }
-    };
-
     try {
       const response = await axios.request(options);
       console.log(response.data);
       setWord(response.data); 
       setFreq(response.data.frequency.perMillion)
-
-      const response2 = await axios.request(options2);
-      console.log(response2.data);
-      setDef(response2.data.definitions); 
       
     } catch (error) {
       console.error(error);
@@ -45,7 +31,7 @@ function App() {
   }
 
   useEffect(()=>{
-    getData("cat")
+    getData()
 
   },[])
 
@@ -54,7 +40,7 @@ function App() {
     <div className="App">
       <div className="wrapper">
         <div className="container">
-          <div class="head">Etycurious: The Curious Etymologist</div>
+          <div>Etycurious: The Curious Etymologist</div>
           <h1></h1>
           <div className="search-box">
             <input type="text" placeholder="Type the word here..." id="inp-word" />
@@ -69,18 +55,6 @@ function App() {
             <div className="details">Word:  
               {" "+ word.word}
             </div>
-
-            <p className="word-meaning">Definitions:{" "}</p>
-              {def.map((eachDef,i)=>{
-              return(
-                <h6 className="defs" key={i}>{i+1}: {eachDef.definition}</h6>
-              ) 
-              })}
-
-
-
-
-
             <p className="word-meaning">Frequency of word per million:{" "}
               <strong>{ freq}</strong>
             </p>
